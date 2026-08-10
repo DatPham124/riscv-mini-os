639a6509da1fd7c02050ae2b536695ce2ccbb5de
DatPham124 <ppap1242003@gmail.com>
2026-08-10 09:00:17 +0700

    wip(Disk I/O): utility functions to kernel.h

--- kernel.c
+++ kernel.c
@@ -401,6 +401,26 @@ void handle_trap(struct trap_frame *f)
 
     WRITE_CSR(sepc, user_pc);
 }
+// Các hàm sau dùng để thao tác với các thanh ghi phần cứng Virtio thông qua cơ chế MMIO (Memory - Mapped I/O)
+
+// ĐỌc 1 số 32 bit hoặc 64 bit từ một thanh ghi của thiết bị Virtio
+uint32_t virtio_reg_read32(unsigned offset) {
+    return *((volatile uint32_t *) (VIRTIO_BLK_PADDR + offset));
+}
+
+uint64_t virtio_reg_read64(unsigned offset) {
+    return *((volatile uint64_t *) (VIRTIO_BLK_PADDR + offset));
+}
+
+// Ch
+
+void virtio_reg_write32(unsigned offset, uint32_t value) {
+    *((volatile uint32_t *) (VIRTIO_BLK_PADDR + offset)) = value;
+}
+
+void virtio_reg_fetch_and_or32(unsigned offset, uint32_t value) {
+    virtio_reg_write32(offset, virtio_reg_read32(offset) | value);
+}
 
 void kernel_main(void)
 {
--- kernel.h
+++ kernel.h
@@ -108,73 +108,91 @@ struct process procs[PROCS_MAX];
 #define SECTOR_SIZE 512
 #define VIRTQ_ENTRY_NUM 16
 #define VIRTIO_DEVICE_BLK 2
+// Địa chỉ RAM nơi QEMU đặt thiết bị ổ cứng
 #define VIRTIO_BLK_PADDR 0x10001000
+// Nút đọc chữ ký thiết bị
 #define VIRTIO_REG_MAGIC 0x00
+// Đọc phiên bản Virtio
 #define VIRTIO_REG_VERSION 0x04
+// Đọc loại thiết bị
 #define VIRTIO_REG_DEVICE_ID 0x08
 #define VIRTIO_REG_PAGE_SIZE 0x28
+// Chọn hàng đợi muốn cấu hình
 #define VIRTIO_REG_QUEUE_SEL 0x30
 #define VIRTIO_REG_QUEUE_NUM_MAX 0x34
+// Báo cho thiết bị kích thước hàng đợi
 #define VIRTIO_REG_QUEUE_NUM 0x38
+// Ghi địa chỉ RAM của Virtqueue vào đây để QEMU biết đường tìm đến
 #define VIRTIO_REG_QUEUE_PFN 0x40
 #define VIRTIO_REG_QUEUE_READY 0x44
+// Nút báo hiệu cho có việc mới trong Avail Ring
 #define VIRTIO_REG_QUEUE_NOTIFY 0x50
 #define VIRTIO_REG_DEVICE_STATUS 0x70
 #define VIRTIO_REG_DEVICE_CONFIG 0x100
+
+// Các cờ báo trạng thái kết nối
 #define VIRTIO_STATUS_ACK 1
 #define VIRTIO_STATUS_DRIVER 2
 #define VIRTIO_STATUS_DRIVER_OK 4
+
 #define VIRTQ_DESC_F_NEXT 1
 #define VIRTQ_DESC_F_WRITE 2
 #define VIRTQ_AVAIL_F_NO_INTERRUPT 1
 #define VIRTIO_BLK_T_IN 0
 #define VIRTIO_BLK_T_OUT 1
 
-// Virtqueue Descriptor Table entry
+// Virtqueue Descriptor Table entry (như tờ giấy mô tả 1 yêu cầu phần cứng)
 struct virtq_desc
 {
-    uint64_t addr;
-    uint32_t len;
-    uint16_t flags;
-    uint16_t next;
+    uint64_t addr;  // Địa chỉ RAM chứa dữ liệu
+    uint32_t len;   // Độ dài vùng nhớ
+    uint16_t flags; // VIRTQ_DESC_F_NEXT (báo có desc nối tiếp) / VIRTQ_DESC_F_WRITE (báo cho QEMU ghi vào)
+    uint16_t next;  // Chỉ số (index) của Desc tiếp theo trong chuỗi Descriptor Chain
 } __attribute__((packed));
 
 // Virtqueue Available Ring.
 struct virtq_avail
 {
     uint16_t flags;
-    uint16_t index;
-    uint16_t ring[VIRTQ_ENTRY_NUM];
+    uint16_t index;                 // Vị trí đơn hàng mới nhất
+    uint16_t ring[VIRTQ_ENTRY_NUM]; // Mảng chứa các ID đại diện cho chuỗi Descriptor
 } __attribute__((packed));
 
+// Hôp thư nhận các yêu cầu đã hoàn thành từ QEMU
 // Virtqueue Used Ring entry.
-struct virtq_used_elem {
-    uint32_t id;
-    uint32_t len;
+struct virtq_used_elem
+{
+    uint32_t id;  // ID của chuỗi Descriptor đã hoàn thành
+    uint32_t len; // Số byte QEMU đã xử lý
 } __attribute__((packed));
 
 // Virtqueue Used Ring.
-struct virtq_used {
+struct virtq_used
+{
     uint16_t flags;
-    uint16_t index;
+    uint16_t index; // Vị trí báo kết quả mới nhất của QEMU
     struct virtq_used_elem ring[VIRTQ_ENTRY_NUM];
 } __attribute__((packed));
 
 // Virtqueue.
-struct virtio_virtq {
-    struct virtq_desc descs[VIRTQ_ENTRY_NUM];
-    struct virtq_avail avail;
-    struct virtq_used used __attribute__((aligned(PAGE_SIZE)));
+struct virtio_virtq
+{
+    struct virtq_desc descs[VIRTQ_ENTRY_NUM];   //Bảng chứa 16 Discriptor
+    struct virtq_avail avail;                   //Avail Ring
+    struct virtq_used used __attribute__((aligned(PAGE_SIZE))); //Used Ring (phải căn chỉnh đúng 4KB RAM)
+    
+    
     int queue_index;
     volatile uint16_t *used_index;
     uint16_t last_used_index;
 } __attribute__((packed));
 
-// Virtio-blk request.
-struct virtio_blk_req {
-    uint32_t type;
-    uint32_t reserved;
-    uint64_t sector;
-    uint8_t data[512];
-    uint8_t status;
+// Virtio-blk request (đơn xin Đọc/Ghi đĩa).
+struct virtio_blk_req
+{
+    uint32_t type;      // VIRTIO_BLK_T_IN (0: Đọc từ đĩa) hoặc VIRTIO_BLK_T_OUT (1: Ghi vào đĩa)
+    uint32_t reserved;  // Ô dự phòng (luôn bằng 0)
+    uint64_t sector;    // Vị trí Sector (khối 512B) trên đĩa muốn thao tác
+    uint8_t data[512];  // Bộ nhớ đệm chứa 512 byte dữ liệu
+    uint8_t status;     // Nơi QEMU trả kết quả (0 = Thành công)
 } __attribute__((packed));
