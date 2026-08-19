3f4e09a1c096482bc239e517f0533e274d18193b
DatPham124 <ppap1242003@gmail.com>
2026-08-19 08:42:36 +0700

    wip(File System): Create a disk image (tar file)

--- run.sh
+++ run.sh
@@ -13,9 +13,11 @@ OBJCOPY=llvm-objcopy
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c common.c
 
+(cd disk && tar cf ../disk.tar --format=ustar *.txt) 
+
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -d unimp,guest_errors,int,cpu_reset -D qemu.log \
-    -drive id=drive0,file=lorem.txt,format=raw,if=none \
+    -drive id=drive0,file=disk.txt,format=raw,if=none \
     -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
     -kernel kernel.elf
